70cd5e9be3310d3e5339cdca97384bbbdc91a268
samran sajjad <samransajjad23@gmail.com>
2023-03-27 15:19:36 +0500

    Email-Verification: 1.add routes in app.js 2.give path of the component to render 3.complete design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import ForgetPassword from './Components/ForgetPassword';
+import Email from './Components/EmailVerification/Email';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App" class='flex justify-center mt-[8px]'>
-      <ForgetPassword/>
+
+
+      <BrowserRouter>
+      <Routes>
+        <Route  path='/forgetpassword' element={<ForgetPassword/>}/>
+        <Route path='/email' element={<Email/>}/>
+      </Routes>
+      </BrowserRouter>
+
+
+
+
+      {/* <ForgetPassword/>
+      <Email/> */}
     </div>
   );
 }
