3204590d0e1503ed66f602604bbcdf4d39258405
samran sajjad <samransajjad23@gmail.com>
2023-03-30 16:08:19 +0500

    ForgetPassword: 1.add breakpoints into tailwindconfig file 2.for small screen responsive done

--- src/components/authComponents/ForgetPassword/ForgetPassword.js
+++ src/components/authComponents/ForgetPassword/ForgetPassword.js
@@ -8,13 +8,13 @@ function ForgetPassword () {
        alert('you clicked me')
     }
     return (
-      <div className='container' class='h-[600px] w-[90%] flex items-center justify-center' >
-        <div className='leftside' class='h-[560px] w-[45%]  flex flex-col  items-center justify-around' >
-            <div className='mycareer' class='h-[100px] w-[90%] flex items-center justify-start' >
+      <div className='container' class='h-[600px] w-[90%] flex items-center justify-center  sm:w-[100%]' >
+        <div className='leftside' class='h-[560px] w-[45%]  flex flex-col  items-center justify-around sm:h-[600px] sm:w-[100%]' >
+            <div className='mycareer' class='h-[100px] w-[90%] flex items-center justify-start ' >
                 <img src={mycareer} class='h-auto w-[150px]' />
             </div>
-            <div className='forget' class='h-[250px] w-[90%] flex flex-col items-center justify-around' >
-              <h2 class='font-bold text-2xl' >Forget your Password?</h2>
+            <div className='forget' class='h-[250px] w-[90%] flex flex-col items-center justify-around  sm:h-[300px]' >
+              <h2 class='font-bold text-2xl sm:text-md' >Forget your Password?</h2>
               <p class='text-[#737373] mb-4 ' >Enter the email associated with your <br/>
                 account and we'll send an email with <br/>
                 instructions to reset your password.</p>
@@ -24,11 +24,11 @@ function ForgetPassword () {
                 </div>
                 <button onClick={handleClick} class='h-[40px] w-[250px] bg-[#0575E6] text-white rounded-md ' >Send Request</button>
             </div>
-            <div class='h-[120px] w-[90%]  flex items-end' >
-                    <p class='text-[#8A8A8A]' >© 2023 My Career Guidance. All Rights Reserved</p>
+            <div class='h-[120px] w-[90%]  flex items-end sm:h-[100px]' >
+                    <p class='text-[#8A8A8A] sm:text-[13px] ' >© 2023 My Career Guidance. All Rights Reserved</p>
                 </div>
         </div>
-        <div className='rightside' class=' bg-cover    rounded-md' >
+        <div className='rightside' class=' bg-cover    rounded-md sm:hidden' >
          <img src={image} class='h-[560px] w-[100%]' />
         </div>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+      screens: {
+        'sm': {'min':'320px','max':'480px'},
+        // => @media (min-width: 640px) { ... }
+  
+        'md': {'min':'481px','max':'768px'},
+        // => @media (min-width: 1024px) { ... }
+  
+        'lg': {'min':'769px','max':'1024px'},
+        // => @media (min-width: 1280px) { ... }
+
+        'xl': {'min':'1025px','max':'1200px'},
+        // => @media (min-width: 1280px) { ... }
+      },
   },
   plugins: [],
 }
