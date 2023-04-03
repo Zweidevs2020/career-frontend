84f2932785e6b27b47425ad37c51ef2e06efb22c
samran sajjad <samransajjad23@gmail.com>
2023-04-03 13:53:56 +0500

    Email-Verification: 1.add responsiveness according to breakspoints

--- src/components/authComponents/EmailVerification/EmailVerification.jsx
+++ src/components/authComponents/EmailVerification/EmailVerification.jsx
@@ -4,18 +4,18 @@ import image from '../../../assets/image.png';
 
 const EmailVerification = () => {
   return (
-         <div className='Email' class='h-[630px] w-[90%] flex items-center justify-center' >
-            <div className='left' class='h-[560px] w-[45%] flex flex-col  items-center justify-around' >
-            <div className='mycareer' class='h-[100px] w-[90%] flex items-center justify-start' >
-                <img src={mycareer} class='h-auto w-[150px]' />
+         <div className='Email' class='h-[655px] w-[100%]  flex items-center justify-center sm:w-[100%] sm:h-[560px] md:w-[100%] md:h-[560px] lg:w-[100%] lg:h-[560px] xl:w-[100%] xl:h-[100%]' >
+          <div  class='h-[560px] w-[45%]  flex flex-col justify-between sm:w-[100%] md:w-[100%] lg:w-[65%]  ' >
+            <div className='mycareer' class='h-[100px] w-[90%] sm:w-[100%]  flex items-center justify-start' >
+                <img src={mycareer} class='h-auto w-[150px] sm:ml-2 md:ml-2 lg:ml-2' />
             </div>
-            <div className='forget' class='h-[339px] w-[90%] flex flex-col items-center justify-around' >
+            <div className='forget' class='h-[339px] w-[90%] sm:w-[100%]  flex flex-col items-center justify-evenly' >
                 <div class='h-[90px] w-[85%]  flex flex-col ml-9 justify-start gap-1' >
                 <h2 class='font-bold text-2xl' >Email Verification</h2>
               <p class='text-[#737373] mb-4 ' >Please enter the 4-digit verification code
               that was sent to your email</p>
                 </div>
-                <div class='h-[100px] w-[85%] ml-9 flex items-center justify-between px-5 py-5 ' >
+                <div class='h-[100px] w-[85%]  ml-9  flex items-center justify-between px-5 py-5 sm:px-3' >
                 <input type='text' name='text' placeholder='4' class='h-[50px] w-[54px] bg-[#DADADA] rounded-md px-5'  />
                 <input type='text' name='text' placeholder='3' class='h-[50px] w-[54px] bg-[#DADADA] rounded-md  px-5'  />
                 <input type='text' name='text' placeholder='' class='h-[50px] w-[54px] bg-[#DADADA] rounded-md px-5 '  />
@@ -23,12 +23,11 @@ const EmailVerification = () => {
                 </div>
                 <button class='h-[40px] w-[250px] bg-[#0575E6] text-white rounded-md ml-9 ' >Continue</button>
             </div>
-            <div class='h-[120px] w-[90%]  flex items-end' >
-                    <p class='text-[#8A8A8A]' >© 2023 My Career Guidance. All Rights Reserved</p>
+            <div class='h-[120px] w-[90%] sm:w-[100%] flex items-end' >
+                    <p class='text-[#8A8A8A] sm:text-[15px] sm:ml-2 md:ml-2 lg:ml-2' >© 2023 My Career Guidance. All Rights Reserved</p>
                 </div>
-
-            </div>
-            <div className='rightside' class=' bg-cover    rounded-md' >
+                </div>
+            <div className='rightside' class=' bg-cover    rounded-md sm:hidden md:hidden' >
          <img src={image} class='h-[560px] w-[100%]' />
         </div>
       </div>
--- src/components/authComponents/ForgetPassword/ForgetPassword.js
+++ src/components/authComponents/ForgetPassword/ForgetPassword.js
@@ -8,27 +8,27 @@ function ForgetPassword () {
        alert('you clicked me')
     }
     return (
-      <div className='container' class='h-[600px] w-[90%] flex items-center justify-center  sm:w-[100%]' >
-        <div className='leftside' class='h-[560px] w-[45%]  flex flex-col  items-center justify-around sm:h-[600px] sm:w-[100%]' >
+      <div className='container' class='h-[655px] w-[100%]  flex items-center justify-center  sm:w-[100%] sm:h-[560px] md:w-[100%] md:h-[560px] lg:w-[100%] lg:h-[560px] xl:w-[100%] xl:h-[560px] ' >
+        <div className='leftside' class='h-[560px] w-[45%]  flex flex-col  items-center justify-around sm:h-[600px] sm:w-[100%] md:w-[100%] lg:w-[65%]  ' >
             <div className='mycareer' class='h-[100px] w-[90%] flex items-center justify-start ' >
                 <img src={mycareer} class='h-auto w-[150px]' />
             </div>
-            <div className='forget' class='h-[250px] w-[90%] flex flex-col items-center justify-around  sm:h-[300px]' >
+            <div className='forget' class='h-[250px] w-[90%]  lg:h-[330px] lg:justify-evenly flex flex-col items-center justify-around  sm:h-[300px]' >
               <h2 class='font-bold text-2xl sm:text-md' >Forget your Password?</h2>
               <p class='text-[#737373] mb-4 ' >Enter the email associated with your <br/>
                 account and we'll send an email with <br/>
                 instructions to reset your password.</p>
-                <div className='input' class=' h-[40px] w-[250px] flex items-center justify-center rounded-md border-solid border-2 border-gray-400' >
-                <img src={emailIcon} class='h-[18px]'/>
-                <input type='text' name='text' placeholder='Email Address' class='h-[30px] w-[220px] '  />
+                <div className='input' class=' h-[40px] w-[250px] flex items-center justify-around rounded-md border-solid border-2 border-gray-400 ' >
+                <img src={emailIcon} class='h-[16px]'/>
+                <input type='text' name='text' placeholder='Email Address' class='h-[30px] w-[200px] '  />
                 </div>
                 <button onClick={handleClick} class='h-[40px] w-[250px] bg-[#0575E6] text-white rounded-md ' >Send Request</button>
             </div>
             <div class='h-[120px] w-[90%]  flex items-end sm:h-[100px]' >
-                    <p class='text-[#8A8A8A] sm:text-[13px] ' >© 2023 My Career Guidance. All Rights Reserved</p>
+                    <p class='text-[#8A8A8A] sm:text-[13px] lg:text-[14px] ' >© 2023 My Career Guidance. All Rights Reserved</p>
                 </div>
         </div>
-        <div className='rightside' class=' bg-cover    rounded-md sm:hidden' >
+        <div className='rightside' class=' bg-cover    rounded-md sm:hidden md:hidden ' >
          <img src={image} class='h-[560px] w-[100%]' />
         </div>
         
