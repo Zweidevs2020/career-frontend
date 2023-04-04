87e5482d301a0d09622aad4f1242503c822f9978
samran sajjad <samransajjad23@gmail.com>
2023-04-04 11:43:54 +0500

    Dashboard: 1. add navbar.jsx file and import in careerdashboard file 2.complete navbar

--- src/components/Careerdashboard/CareerDashboard.jsx
+++ src/components/Careerdashboard/CareerDashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar';
 
 const CareerDashboard = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='main' class='h-[100vh] w-[100%] bg-[#F5F5F5] grid grid-cols-5 grid-rows-[90px,560px] gap-[2px]' >
+      <div className='aside' class='bg-[#F8FAFC] col-span-1 row-span-2 ' ></div>
+      <div className='navbar' class='bg-[#F8FAFC] col-span-4 flex items-center justify-around ' >
+        <Navbar/>
+      </div>
+      <div className='career' class='bg-white col-span-3 ' ></div>
+      <div className='rightaside' class='bg-white' ></div>
     </div>
   )
 }
---
+++ src/components/Careerdashboard/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import navImg from '../../../assets/navImg.svg'
+const Navbar = () => {
+  return (
+    <>
+    <div class='h-[80px] w-[45%]  flex items-center justify-center' >
+    <p class='text-[#1476B7] text-[22px] ' >Hello <strong> Bruno Fernandes</strong>, welcome back!</p>
+    </div>
+     <div class='h-[80px] w-[45%]  flex items-center justify-end' >
+        <div class='bg-[#1476B7] h-[65px] w-[60%] mr-8 rounded-xl flex items-center justify-around'>
+            <img src={navImg} class='h-[55px]' />
+            <div class='h-[55px] w-[70%] flex flex-col justify-center' >
+                <p class='text-[#fff]' >Bruno Fernandes</p>
+                <p class='text-[#fff]'>Bruno@gmail.com</p>
+            </div>
+        </div>
+     </div>
+     </>
+  )
+}
+
+export default Navbar
