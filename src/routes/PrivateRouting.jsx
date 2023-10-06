59fdb17f7e42714445b7f871bedf5319b24272f6
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-10-06 13:06:20 +0500

    testing commit

--- src/routes/PrivateRouting.jsx
+++ src/routes/PrivateRouting.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { getToken } from "../utils/LocalStorage";
 const PrivateRoute = ({ children }) => {
-  return getToken() ? <>{children}</> : <Navigate to="/" />;
+  return getToken() ? <>{children}</> : <Navigate to="/" />; 
 };
 
 export default PrivateRoute;
