01553ad38a00e559a590f36161718531dab690fe
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-10-10 16:44:15 +0500

    Steps issue fixed

--- src/components/CvCoverLetter/Steps/Steps.jsx
+++ src/components/CvCoverLetter/Steps/Steps.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Steps.css";
 
-const Steps = ({ current, setCurrent, currentStep }) => {
-console.log("curremt setcurrent",current,currentStep)
+const Steps = ({ current, setCurrent, currentStep,isCvComplete }) => {
+console.log("curremt setcurrent",current,setCurrent,currentStep,isCvComplete)
 
 
   return (
@@ -34,7 +34,7 @@ console.log("curremt setcurrent",current,currentStep)
           </div>
           <div
             className={
-              currentStep >= 1
+              currentStep >= 1 
                 ? "step-item-content step-item-content-active"
                 : "step-item-content step-item-content-nonActive"
             }
@@ -57,28 +57,28 @@ console.log("curremt setcurrent",current,currentStep)
               className="step-item"
               key={index + 2}
               onClick={() => {
-                if (currentStep >= index + 2) {
+                if (currentStep >= index + 2 || isCvComplete) {
                   setCurrent(index + 2);
                 }
               }}
             >
               <div
                 className={
-                  currentStep >= index + 2
+                  currentStep >= index + 2 || isCvComplete
                     ? "step-item-line  step-item-line-active"
                     : "step-item-line  step-item-line-nonActive"
                 }
               />
               <div
                 className={
-                  currentStep >= index + 2
+                  currentStep >= index + 2 || isCvComplete
                     ? "step-icon-style step-icon-style-active"
                     : "step-icon-style step-icon-style-nonActive"
                 }
               >
                 <span
                   className={
-                    currentStep >= index + 2
+                    currentStep >= index + 2 || isCvComplete
                       ? "step-icon-text step-icon-text-active"
                       : "step-icon-text step-icon-text-nonActive"
                   }
@@ -88,7 +88,7 @@ console.log("curremt setcurrent",current,currentStep)
               </div>
               <div
                 className={
-                  currentStep >= index + 2
+                  currentStep >= index + 2 || isCvComplete
                     ? "step-item-content step-item-content-active"
                     : "step-item-content step-item-content-nonActive"
                 }
