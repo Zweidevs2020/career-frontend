0446727e6fdf5ea029199da61c119c107de2945a
subhan-sikandar <subhan.sikandar@zweidevs.com>
2025-08-22 17:17:38 +0500

    fixing

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -30,7 +30,7 @@ import styles from "./CheckoutView.module.css";
 
 function CheckoutView() {
   const stripePromise = loadStripe(
-    process.env.STRIPE_PUBLIC_KEY
+    `${process.env.REACT_APP_STRIPE_PUBLIC_KEY}`
   );
 
   return (
