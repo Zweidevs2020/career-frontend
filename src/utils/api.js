74338bc6ae31b44b3dfd0e58b6c7e21ce904c3b0
subhan-sikandar <subhan.sikandar@zweidevs.com>
2025-12-17 21:47:44 +0500

    fix api.js  401 issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,12 @@
 /* eslint-disable no-console */
 import axios from "axios";
-// import { getToken } from "./LocalStorage"; // No longer needed
+import { getToken } from "./LocalStorage";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 console.log("Base URL:", BASE_URL);
 console.log("Axios Version:", process.env.REACT_APP_BASE_URL);
 const baseInstance = axios.create({
   baseURL: BASE_URL,
 });
-
-// Function to get token from cookies - Copied from conselorDashboard.jsx
-const getCookie = (name) => {
-  const cookies = document.cookie.split("; ");
-  for (const cookie of cookies) {
-    const [key, value] = cookie.split("=");
-    if (key === name) {
-      return value;
-    }
-  }
-  return null;
-};
 
 export const getApiWithoutAuth = async (url) => {
   try {
@@ -114,12 +102,28 @@ export const postApiWithoutAuth = async (url, body) => {
   }
 };
 
+
+const getCookie = (name) => {
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+};
 export const deleteMultipleStudents = async (student_ids) => {
-  await setApiHeader();
+  const token = getCookie("conselorToken");
   try {
     const res = await baseInstance.post(
-      `${BASE_URL}user/counselor/delete-multiple/`,
-      { student_ids: student_ids }
+      `user/counselor/delete-multiple/`,
+      { student_ids: student_ids },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
     );
     return {
       data: res.data,
@@ -130,19 +134,19 @@ export const deleteMultipleStudents = async (student_ids) => {
   }
 };
 
+
 export const postFormDataAPI = async (url, body) => {
   baseInstance.defaults.headers.common["Content-Type"] = "multipart/form-data";
   await setApiHeader();
   try {
     const res = await baseInstance.post(url, body);
     return res.data;
-  }
-  catch (err) {
+  } catch (err) {
     return err.response;
   }
 };
 
 const setApiHeader = async () => {
   baseInstance.defaults.headers.common["Authorization"] =
-    "Bearer " + getCookie("conselorToken");
+    "Bearer " + getToken();
 };
